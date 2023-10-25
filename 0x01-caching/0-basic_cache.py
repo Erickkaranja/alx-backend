880ac5cbca3e9fdf67a916d0ec57f3d1452d5135
Erickkaranja <karanja99erick@gmail.com>
2023-10-25 18:59:58 +0300

    updating task 1

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''creating a basic cache module'''
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''creates a basic cache.'''
+    def put(self, key, item):
+        '''adds a key value pair into the cache'''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''retrieves value from the cache.'''
+        if key is not None:
+            return self.cache_data.get(key, None)
+        return None
